8a8721cefcc47c6e3155d69b2a7e2d8442fc7f4d
mitaly-dev <mitalyy.dev@gmail.com>
2022-08-16 09:56:03 -0700

    input validation

--- js/login.js
+++ js/login.js
@@ -1,13 +1,13 @@
-let login = document.getElementById("login-btn");
-login.addEventListener("click", function () {
+document.getElementById("login-btn").addEventListener("click", function () {
   let email = document.getElementById("email");
   let emailValue = email.value;
   let password = document.getElementById("password");
   let passwordValue = password.value;
   email.value == "";
   password.value == "";
-  login.style.backgroundColor = "red";
   if (emailValue.includes("@gmail.com") == true && passwordValue == "mybank") {
     window.location.href = "./bank-account.html";
+  } else {
+    return alert("Please provide your valid email and password");
   }
 });
